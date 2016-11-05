05320cbd79ba677f6e4679133db7a079b25d470d
Yoon Kim <yoonkim@berkeley.edu>
2016-11-05 08:39:18 -0700

    profile complete, merge with Simon

--- js/profile.js
+++ js/profile.js
@@ -1,30 +1,23 @@
 google.load('visualization', '1', {'packages': ['geochart', 'corechart']});
 $(document).ready(function() {
-
-  $(".forcegraph").hide();
-  $(".info").hide();
   google.setOnLoadCallback(drawMap);
-  google.charts.setOnLoadCallback(drawRegionsMap);
-  var chart;
-  var data;
-  var options;
+
   function drawMap() {
 
-    data = new google.visualization.DataTable();
-
-    data.addColumn('number', 'latitude');
-    data.addColumn('number', 'longitude');
-    data.addColumn('string', 'name');
-    data.addColumn('number', 'color');
-    data.addColumn('number', 'value', 'value');
-    data.addColumn({type:'string', role:'tooltip'});
+    var data = google.visualization.arrayToDataTable([
+        ['Region',   'Donated Amount ($)'],
+        ['Haiti',  42],
+        ['Middle Africa', 12],
+        ['Ethiopia', 23]
+      ]);
 
     options = {
       displayMode: 'markers',
       enableRegionInteractivity: 'false',
       backgroundColor: 'transparent',
       defaultColor: '#f5f5f5',
-      sizeAxis: {minSize:8,  maxSize: 8},
+      sizeAxis: {minSize:8,  maxSize: 12},
+      colorAxis: {colors: ['#98fb98', 'green']}
     };
 
   chart = new google.visualization.GeoChart(document.getElementsByClassName("chart_div")[0]);
